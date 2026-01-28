7a5934cb1b71f719d82b08dbc8d1706d57957c7a
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2026-01-28 13:07:16 -0500

    updated the trustpilot script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -269,9 +269,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
-              var a=d.createElement(s);a.async=1;a.src=r;a.type='text/javascript';var f=d.getElementsByTagName(s)[0];
-              f.parentNode.insertBefore(a,f)})(window,document,'script','https://invitejs.trustpilot.com/tp.min.js','tp');
-              tp('register', 'qdeqUpdF7WxPCJEm');
+            a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+            f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+            tp('register', 'WAyQ5n67MfRznVix');
             `,
           }}
         />
